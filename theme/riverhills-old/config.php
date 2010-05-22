f89ded594825848e8f1ef68d6828e1f264d11b41
matt.meisberger@gmail.com <matt.meisberger@gmail.com@1ff7bc8b-0ae6-490c-ae3b-7ac585c1b48c>
2010-05-22 16:04:58 +0000

    a few theme changes needed for dropdown changes
    git-svn-id: https://free2.projectlocker.com/riverhills/moodle/svn/trunk@46 1ff7bc8b-0ae6-490c-ae3b-7ac585c1b48c

--- theme/riverhills-old/config.php
+++ theme/riverhills-old/config.php
@@ -6,7 +6,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 
-$THEME->sheets = array('mods', 'calendar');
+$THEME->sheets = array('mods', 'calendar', 'cssmenu');
 
 /// This variable is an array containing the names of all the 
 /// stylesheet files you want included in this theme, and in what order
