eb1529a6fccaa0992344f0a71ed8c98cd9cf4953
matt.meisberger@gmail.com <matt.meisberger@gmail.com@1ff7bc8b-0ae6-490c-ae3b-7ac585c1b48c>
2011-03-22 22:36:39 +0000

    moodle_optimization module incidentals
    git-svn-id: https://free2.projectlocker.com/riverhills/moodle/svn/trunk@79 1ff7bc8b-0ae6-490c-ae3b-7ac585c1b48c

--- theme/rhcc-new/styles.php
+++ theme/rhcc-new/styles.php
@@ -9,7 +9,9 @@
 
     $lifetime  = 600;                                   // Seconds to cache this stylesheet
     $nomoodlecookie = true;                             // Cookies prevent caching, so don't use them
-    require_once("../../config.php");                   // Load up the Moodle libraries
+    if (!defined('MO_CRON')) {
+        require_once("../../config.php");                   // Load up the Moodle libraries
+    }
     $themename = basename(dirname(__FILE__));           // Name of the folder we are in
     $forceconfig = optional_param('forceconfig', '', PARAM_FILE);   // Get config from this theme
     $lang        = optional_param('lang', '', PARAM_FILE);          // Look for styles in this language
--- theme/rhcc2/styles.php
+++ theme/rhcc2/styles.php
@@ -9,9 +9,11 @@
 
     $lifetime  = 600;                                   // Seconds to cache this stylesheet
     $nomoodlecookie = true;                             // Cookies prevent caching, so don't use them
-    require_once("../../config.php");                   // Load up the Moodle libraries
+    if (!defined('MO_CRON')) {
+        require_once("../../config.php");                   // Load up the Moodle libraries
+    }
     $themename = basename(dirname(__FILE__));           // Name of the folder we are in
-    $forceconfig = optional_param('forceconfig', '', PARAM_FILE);   // Get config from this theme
+    $forceconfig = optional_param('forceconfig', 'rhcc2', PARAM_FILE);   // Get config from this theme
     $lang        = optional_param('lang', '', PARAM_FILE);          // Look for styles in this language
 
     style_sheet_setup(time(), $lifetime, $themename, $forceconfig, $lang);
