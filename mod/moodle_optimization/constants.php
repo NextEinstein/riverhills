8828e25c621d784795cec1a54ebb0af31c74b459
Matt <matt@matt-VirtualBox.(none)>
2011-05-11 01:37:37 -0500

    delete moodle optimization to re-integrate

--- mod/moodle_optimization/constants.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-define('MO_CACHED_FILES_BASE_DIR', 'cached');
--- mod/moodle_optimization/version.php
+++
@@ -1,9 +0,0 @@
-<?php // $Id: version.php,v 1.17.2.2 2008/07/11 02:54:54 moodler Exp $
-
-/////////////////////////////////////////////////////////////////////////////////
-///  Code fragment to define the version of label
-///  This fragment is called by moodle_needs_upgrading() and /admin/index.php
-/////////////////////////////////////////////////////////////////////////////////
-
-$module->version  = 2011031302;  // The current module version (Date: YYYYMMDDXX)
-$module->cron     = 1;    // Period for cron to check this module (secs)
